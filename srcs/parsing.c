a6edbe0f93bba688dba8f466fab94c5eaa7b1172
Arnalove <chansarelarno@gmail.com>
2023-06-28 19:02:59 +0200

    parsing

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,8 +6,20 @@
 /*   By: achansar <achansar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 12:08:04 by achansar          #+#    #+#             */
-/*   Updated: 2023/06/28 18:57:37 by achansar         ###   ########.fr       */
+/*   Updated: 2023/06/28 19:01:24 by achansar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#
+void	parsing(t_game *game, int ac, char **av)
+{
+	if (ac != 2)
+		errors(game, 1, NULL);
+	extension_check(game, av[1]);
+	read_map(game, av[1]);
+	game->s_map = ft_split(game->map, '\n');
+	error_map(game, game->map);
+	rgb_check(game, game->floor, &game->color_floor);
+	rgb_check(game, game->roof, &game->color_roof);
+	init_player_pos(game);
+	filling_sprite_tab(game);
+}
