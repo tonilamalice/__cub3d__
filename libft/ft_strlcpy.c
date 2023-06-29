251aa1d7007c00ef1bd0bb646b0604d47f18e9cb
Antoine De Bast <ade-bast@mi-r4-p1.s19.be>
2023-06-29 14:10:47 +0200

    sorting libft's file out

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,52 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-bast <ade-bast@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/04 18:24:50 by achansar          #+#    #+#             */
-/*   Updated: 2023/06/29 14:07:14 by ade-bast         ###   ########.fr       */
+/*   Created: 2023/06/29 14:05:43 by ade-bast          #+#    #+#             */
+/*   Updated: 2023/06/29 14:06:53 by ade-bast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub.h"
 
-char	*ft_strdup(const char *s1)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	int		i;
-	char	*s2;
+	size_t	i;
+	size_t	res;
 
+	res = ft_strlen(src);
+	if (dstsize == 0)
+		return (res);
 	i = 0;
-	s2 = malloc((ft_strlen(s1) + 1) * sizeof(char));
-	if (!s2)
-		return (0);
-	while (s1[i] != '\0')
+	while (src[i] && i < dstsize - 1)
 	{
-		s2[i] = s1[i];
+		dst[i] = src[i];
 		i++;
 	}
-	s2[i] = '\0';
-	return (s2);
-}
-
-char	*ft_strdup_pars(const char *src)
-{
-	int		i;
-	char	*ptr;
-
-	i = 0;
-	while (src[i])
-		i++;
-	ptr = malloc(sizeof(src[0]) * i + 1);
-	if (!ptr)
-		exit(EXIT_FAILURE);
-	i = 0;
-	while (src[i])
-	{
-		ptr[i] = src[i];
-		i++;
-	}
-	ptr[i] = '\0';
-	return (ptr);
+	dst[i] = '\0';
+	return (res);
 }
